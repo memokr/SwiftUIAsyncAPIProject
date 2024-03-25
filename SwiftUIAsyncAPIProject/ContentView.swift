57999089f370bd088d3630a42d3966cb323ba667
memokr <88461561+memokr@users.noreply.github.com>
2024-03-25 12:47:07 +0100

    project created

---
+++ SwiftUIAsyncAPIProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIAsyncAPIProject
+//
+//  Created by Guillermo Kramsky on 25/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Work in progress for API Async Project 👍🏼")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIAsyncAPIProject/SwiftUIAsyncAPIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsyncAPIProjectApp.swift
+//  SwiftUIAsyncAPIProject
+//
+//  Created by Guillermo Kramsky on 25/03/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsyncAPIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
